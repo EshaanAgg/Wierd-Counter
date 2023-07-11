b25b547c03291a6b86e287c1725d4b09a24114a6
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-07-11 23:37:50 +0530

    Add all dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), vue(), svelte(), solidJs()]
+});
